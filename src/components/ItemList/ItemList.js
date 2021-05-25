e63b62bd9efa0d4eeab2665cfa15ad184e7e6ed4
Anastasia Mitina <a.mitina@itransition.com>
2021-05-25 14:04:02 +0300

    feat: created components

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ItemList.css';
+
+export const ItemList = () => {
+    return (
+        <ul className="item-list list-group">
+            <li className="list-group-item">
+                Luke Skywalker
+            </li>
+            <li className="list-group-item">
+                Darth Vader
+            </li>
+            <li className="list-group-item">
+                R2-D2
+            </li>
+        </ul>
+    )
+}
---
+++ src/components/StarshipDetails/StarshipDetails.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './StarshipDetails.css';
+
+export const StarshipDetails = () => {
+
+}
